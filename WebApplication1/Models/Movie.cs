c900413f595d55361233189e414bb7a01bb64a9e
Catchme <mdksankalpa@gmail.com>
2020-08-04 08:30:26 +0930

    Add project files.

---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+
+        public string ContactEmailAddress { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+    }
+}
