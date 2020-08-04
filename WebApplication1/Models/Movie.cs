e4a5180c8459fff0fea989088a39d480a1f176a9
Catchme <mdksankalpa@gmail.com>
2020-08-04 20:50:46 +0930

    Commi email required values

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -22,6 +22,8 @@ namespace WebApplication1.Models
 
 
         [EmailAddress]
+        [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
+
         public string ContactEmailAddress { get; set; }
 
         [ForeignKey("Category")]
